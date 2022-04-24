30ae12aab8e88eba8041201681da8a0a713cf26d
penguin0001 <robynpgreer@gmail.com>
2022-04-24 19:14:35 +0100

    1-4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr, element) {
+    for (let i = 1; i < arguments.length; i++) {
+        if (arr.includes(arguments[i])) {
+            const index = arr.indexOf(arguments[i]);
+            arr.splice(index, 1); 
+        }  
+    }
+    return arr;
 };
 
 // Do not edit below this line
